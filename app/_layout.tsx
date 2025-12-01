d84073bde1aa67694becb94aca86f80b6916e78f
Mahdi <mahdiaghamohammadi28@gmail.com>
2025-12-01 18:19:49 +0330

    Loading and using fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,31 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "IranYekan-Thin": require("@/assets/fonts/Iranyekan-Thin.ttf"),
+    "IranYekan-Light": require("@/assets/fonts/Iranyekan-Light.ttf"),
+    "IranYekan-Regular": require("@/assets/fonts/Iranyekan-Regular.ttf"),
+    "IranYekan-Medium": require("@/assets/fonts/Iranyekan-Medium.ttf"),
+    "IranYekan-Bold": require("@/assets/fonts/Iranyekan-Bold.ttf"),
+    "IranYekan-Extrabold": require("@/assets/fonts/Iranyekan-Extrabold.ttf"),
+    "IranYekan-Black": require("@/assets/fonts/Iranyekan-Black.ttf"),
+    "IranYekan-Extrablack": require("@/assets/fonts/Iranyekan-Extrablack.ttf"),
+    "Poppins-Regular": require("@/assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack />;
 }
