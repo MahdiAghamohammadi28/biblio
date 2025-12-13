f6477eeb7b3ff00ff063c540e8b2645b6db04c9d
Mahdi <mahdiaghamohammadi28@gmail.com>
2025-12-13 19:43:14 +0330

    Add SignUp screen

---
+++ app/(auth)/signup.tsx
@@ -0,0 +1,66 @@
+import SignUpForm from "@/components/authentication/SignUpForm";
+import Btn from "@/components/ui/Btn";
+import { COLORS } from "@/constants/colors";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { moderateScale, scale, verticalScale } from "react-native-size-matters";
+
+export default function SignUp() {
+  const [isEmailSend, setIsEmailSend] = useState<boolean>(false);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {isEmailSend ? (
+        <View style={styles.emailConfirm}>
+          <Text style={styles.emailConfirmText}>
+            یک ایمیل تأیید برات فرستاده شد. لطفاً ایمیلت رو چک کن و روی لینک
+            تأیید بزن
+          </Text>
+          <Btn
+            variant="ghost"
+            width={scale(180)}
+            height={verticalScale(40)}
+            onPress={() => router.replace("/(auth)/signin")}
+            style={{ marginTop: 20 }}
+            fontSize={moderateScale(12)}
+          >
+            بازگست به صفحه ورود
+          </Btn>
+        </View>
+      ) : (
+        <>
+          <Text style={styles.title}>ایجاد حساب کاربری</Text>
+          <SignUpForm onSendConfirmEmail={() => setIsEmailSend(true)} />
+        </>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    direction: "rtl",
+    flex: 1,
+    backgroundColor: COLORS.light.bg,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontFamily: "IranYekan-Extrabold",
+    fontSize: moderateScale(26),
+    marginBottom: verticalScale(12),
+  },
+  emailConfirm: {
+    paddingHorizontal: moderateScale(24),
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  emailConfirmText: {
+    fontFamily: "IranYekan-Bold",
+    fontSize: moderateScale(18),
+    textAlign: "center",
+    marginBottom: verticalScale(12),
+  },
+});
